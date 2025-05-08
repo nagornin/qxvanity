84d348399e90db28db02cecfeec9e175d4f52b29
nagornin <466821@niuitmo.ru>
2025-05-08 05:46:12 +0300

    Don't encode padding when checking a WireGuard key

--- src/wireguard.rs
+++ src/wireguard.rs
@@ -1,5 +1,10 @@
 use crate::generator::{Generator, PrivateKey, PublicKey};
-use base64::{Engine, engine::general_purpose::STANDARD as BASE64_STANDARD};
+use base64::{
+    Engine,
+    engine::general_purpose::{
+        STANDARD as BASE64_STANDARD, STANDARD_NO_PAD as BASE64_STANDARD_NO_PAD,
+    },
+};
 use regex::RegexSet;
 
 pub struct WireGuardGenerator;
@@ -15,7 +20,7 @@ impl Generator for WireGuardGenerator {
     ) -> Option<(Self::PrivateKey, Self::PublicKey)> {
         let private = x25519_dalek::StaticSecret::random();
         let public = x25519_dalek::PublicKey::from(&private);
-        let enc_public = BASE64_STANDARD.encode(public.to_bytes());
+        let enc_public = BASE64_STANDARD_NO_PAD.encode(public.to_bytes());
 
         patterns.is_match(&enc_public).then_some((private, public))
     }
